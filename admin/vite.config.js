e114df838ae8e5e4645881a8e25b4e7a9e92f76e
Aman Singh <amansingh93343@gmail.com>
2025-01-31 20:47:00 +0530

    Remove rollupOptions from Vite build configuration

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,11 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
-    },
-  },
+  
   plugins: [react()],
   server: {
     port: 5174
